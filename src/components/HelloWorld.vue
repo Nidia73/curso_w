da7988db2680608965a5837bb0c30fe40ac83f5b
Nidia Rodriguez <margy73@hotmail.com>
2021-11-30 13:15:40 -0500

    desarrollo terminado

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,56 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div>
+      <h1>Contenido</h1>
+    </div>
   </div>
-</template>
+ </template> 
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      nombre : 'Nidia',
+      mostrar : true,
+      clientes :[{id:1,nombre : 'Nidia'},{id:2,nombre:'Margoth'}],
+      url : 'https://www.google.com',
+      numero1 :0,
+      numero2 :0,
+      resultado :0,
+      tarea : null,
+      tareas : []
+    }
+  },
+  methods :{
+    metodo1(){
+      alert("Click")
+     /*console.log('Ingrese el metodo')*/
+    },
+    sumar(){
+      this.resultado= this.numero1 + this.numero2
+    },
+    restar(){
+      this.resultado= this.numero1 - this.numero2
+    },
+    multiplicar(){
+      this.resultado= this.numero1 * this.numero2
+    },
+    dividir(){
+      this.resultado= this.numero1 / this.numero2
+    },
+    agregarTarea(){
+      alert("Agregar tarea"+this.tarea)
+      this.tareas.push(this.tarea)
+    }
   }
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
@@ -49,8 +62,8 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  color:blue;
+  font-size: 34px;
 }
 a {
   color: #42b983;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ISSFA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/clientes">Clientes</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/proveedores">Proveedores</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/productos">Productos</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/pedido">Pedido</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>    
+    </div>
+  </div>
+</nav>
+</template>
